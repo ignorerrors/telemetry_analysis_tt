2ac74385dd1e51f98b992f77a18186090dd24c69
Zakhar Tikhonov <erroraccount@bk.ru>
2025-09-15 20:56:20 +0400

    Add: setup_layout, export_graphs/stats, about, exit, hot key

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 
 class MainApplication(ttk.Frame):  # Графический интерфейс
@@ -12,10 +12,21 @@ class MainApplication(ttk.Frame):  # Графический интерфейс
         self.parent.iconphoto(True, icon)
 
         self.interface_elements()
+        self.setup_layout()
 
     def interface_elements(self):
         """Создание элементов интерфейса"""
         self.top_level_menu()
+        self.notebook = ttk.Notebook(self.parent) # Рабочая область
+        
+        self.status_var = tk.StringVar() #Статус бар
+        self.status_var.set("Готов к работе") # RIDGE - эффект выпуклой выемки, W - левый край
+        self.status_bar = ttk.Label(self.parent, textvariable=self.status_var, relief=tk.RIDGE, anchor=tk.W)
+
+    def setup_layout(self):
+        """Расстановка элементов в окне"""
+        self.notebook.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+        self.status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
     def top_level_menu(self):
         """Верхнее меню"""
@@ -25,8 +36,9 @@ class MainApplication(ttk.Frame):  # Графический интерфейс
         file_menu = tk.Menu(menubar, tearoff=0)  # Добавил выпадающие окна, без открепления
         menubar.add_cascade(label="Файл", menu=file_menu)
         file_menu.add_command(label="Открыть...", accelerator="Ctrl+O")
+        self.parent.bind('<Control-o>', self.open()) # Горячие клавиши 
         file_menu.add_separator()
-        file_menu.add_command(label="Выход")
+        file_menu.add_command(label="Выход", command=self.exit)
         
         export_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Экспорт", menu=export_menu)
@@ -35,8 +47,35 @@ class MainApplication(ttk.Frame):  # Графический интерфейс
         
         help_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Справка", menu=help_menu)
-        help_menu.add_command(label="О программе")
+        help_menu.add_command(label="О программе", command=self.about)
 
+    def open(self): # Реализовать
+        ...
+
+    def export_graphs(self):
+        """Обработчик кнопки 'Экспорт графиков'"""
+        messagebox.showinfo("Экспорт", "Функция будет реализована...")
+
+    def export_stats(self):
+        """Обработчик кнопки 'Экспорт статистики'"""
+        messagebox.showinfo("Экспорт", "Функция будет реализована...")
+
+    def about(self):
+        """Обработчик кнопки 'О программе'"""
+        about_text = """
+        Анализатор телеметрии
+        Версия 0.1 (Pre-Alpha)
+        
+        Разработано для анализа и визуализации
+        данных телеметрии uav.
+        
+        https://github.com/raiserror/telemetry_analysis_tt
+        """
+        messagebox.showinfo("О программе", about_text)
+
+    def exit(self):
+        """Выход из приложения"""
+        self.parent.quit()
 
 if __name__ == "__main__":  # Запуск для тестирования этого файла
     root = tk.Tk()
